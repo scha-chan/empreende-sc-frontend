a59e7c1a261ab79429838c75a699bafeb60e5e2c
Schasta Jensen <schasta@gmail.com>
2026-03-11 20:31:10 -0300

    Corrigir prerender dos paramêtros

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,13 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'empreendimentos/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
